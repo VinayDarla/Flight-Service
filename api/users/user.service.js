efa50bd5ae71470b26a5c640f3f3ccf9df55f4a2
ngactive99 <ngactive99@gmail.com>
2022-04-28 11:16:27 +0530

    service and controller create method implemeneted

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -1,1 +1,26 @@
-const pool = require("../../config/database")
+const pool = require("../../config/database");
+
+module.exports = {
+    create:(data, callback) => {
+        pool.query(
+            `INSERT INTO REGISTRATION(firstname, lastname, gender, email, password, number)
+            values(?,?,?,?,?,?)`,
+            [
+                data.first_name,
+                data.last_name,
+                data.gender,
+                data.email,
+                data.password,
+                data.number
+            ],
+            ( error, results, fields) => {
+                if(error) {
+                  return callback(error)
+                }
+                return callback(null, results)
+
+            }
+        );
+
+    }
+}
